f2dd24795d67d0f854705230596a5d94b2c89469
Joshua O'bannon <jtobannon@gmail.com>
2020-02-10 09:52:17 -0700

    Add Service for PotterAPI

---
+++ app/services/potter_service.rb
@@ -0,0 +1,26 @@
+class PotterService 
+  def get_house
+    get_json('houses')
+     
+  end
+
+  def get_member(member)
+    get_json()
+  end
+  
+  private
+
+  def conn
+    Faraday.new(url: "https://www.potterapi.com/v1/") do |f|
+      f.adapter Faraday.default_adapter
+      f.params[:access_token] = ENV[:access_token] 
+    end
+  end
+  
+  def get_json(uri)
+    response = conn.get(uri)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+end
+https://www.potterapi.com/v1/houses/5a05e2b252f721a3cf2ea33f?key=$2a$10$voN3YahlYnnnPXz9Pc3OOecHTDuD9AaWfDD3KQbtNVTbYUlYFOXZi
